7d41e25de99ae4cf4382bd06edcfb82e5483f34e
Sarah Drake <jussaradrake@outlook.com>
2019-08-12 11:56:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -2,9 +2,8 @@ def welcome
   puts "Welcome to the Blackjack Table"
 end
 
-
 def deal_card
-card = rand(1..11)
+  card = rand(1..11) #reuse the randomize method by setting it equal to a variable
 end
 
 def display_card_total(card_total)
@@ -16,7 +15,7 @@ def prompt_user
 end
 
 def get_user_input
-user_input = gets.chomp
+  user_input = gets.chomp
 end
 
 def end_game(card_total)
@@ -24,20 +23,18 @@ def end_game(card_total)
 end
 
 def initial_round
-card_one = deal_card
-card_two = deal_card
-card_total = card_one + card_two
-display_card_total(card_total)
-return card_total
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total(card_total) #once you have figured out the sum of your two cards, you can pass the total in as an argumnt so your phrase is displayed.
+  return card_total
 end
 
 def hit?(card_total)
   prompt_user #ask the user if they want to hit or stay
-  user_input = get_user_input #set a variable to use for your if/else
-  #statement
+  user_input = get_user_input #set a variable to use for your if/else statement
   if user_input == "h"
-    card_total += deal_card #increase your former total by the new
-    #number given by dealing another card
+    card_total += deal_card #increase your former total by the new number given by dealing another card
   elsif user_input == "s"
     card_total #return the number and prompt user to hit again
   else
